832fa605e17f5c067dab88bafbe1488bfc0e1195
sevlanov <32263545+sevlanov@users.noreply.github.com>
2017-09-28 14:47:54 +0300

    Тест Изменения группы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupModificationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.learn.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.learn.addressbook.model.GroupData;
+
+public class GroupModificationTest extends TestBase {
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
