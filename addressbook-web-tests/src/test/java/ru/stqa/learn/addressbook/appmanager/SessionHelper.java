2a461f519813ad2a144790a6baad442497cd7f05
sevlanov <32263545+sevlanov@users.noreply.github.com>
2017-09-28 14:25:55 +0300

    Выделены классы-помощники

---
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,21 @@
+package ru.stqa.learn.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SessionHelper {
+    private FirefoxDriver wd;
+    public SessionHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void login(String userName, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(userName);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+}
