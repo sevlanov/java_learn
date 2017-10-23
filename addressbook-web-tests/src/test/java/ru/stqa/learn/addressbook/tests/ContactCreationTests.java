ea1a4903230f68e1fad1504a033e730c6fc2e12f
sevlanov <32263545+sevlanov@users.noreply.github.com>
2017-10-23 16:43:49 +0300

    Задание 13

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/ContactCreationTests.java
@@ -1,25 +1,52 @@
 package ru.stqa.learn.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.learn.addressbook.model.ContactData;
 import ru.stqa.learn.addressbook.model.Contacts;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(new File("src/test/resources/contacts.xml"));
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        String json = "";
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(new FileReader(new File("src/test/resources/contacts.json")),
+                new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact) {
         app.goTo().contactPage();
         Contacts before = app.contact().all();
         File photo = new File("src/test/resources/stru.jpg");
-        ContactData contact = new ContactData().withFirstName("Name").withLastName("Surname")
+        /* ContactData contact = new ContactData().withFirstName("Name").withLastName("Surname")
                 .withNickName("TrueNick").withMobile("8(908)778-80-25").withEmail("test@mail.ru").withBday(11)
-                .withBmonth("April").withByYear(1955).withGroup("test1").withPhoto(photo);
-        app.contact().create(contact, true);
+                .withBmonth("April").withByYear(1955).withGroup("test1").withPhoto(photo); */
+        app.contact().create(contact.withPhoto(photo), true);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
         assertThat(after, equalTo(
